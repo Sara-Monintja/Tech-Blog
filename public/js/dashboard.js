0ee14568f04eac764176ed0123e806adaf00fd34
Sara-M <sara.monintja@gmail.com>
2022-09-10 13:50:54 +0800

    modified dashboard.js

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,8 +1,22 @@
-var createPostBtn = document.getElementById("create-post");
-var form = document.getElementById("new-post-form");
+const createPostBtn = document.getElementById("create-post");
+const form = document.getElementById("new-post-form");
 
 createPostBtn.addEventListener('click', function() {
     // show form
     form.setAttribute('style', 'display: block;')
     createPostBtn.setAttribute('style', 'display: none;')
-})
+});
+
+// if (title && content) {
+//     const response = await fetch(`/api/homeRoutes.js`, {
+//         method: 'POST',
+//         body: JSON.stringify({ title, content }),
+//         // TODO: ADD : headers: ???
+//     });
+
+//     if(response.ok) {
+//         document.location.replace('/profile');
+//     } else {
+//         alert('Failed to save blog');
+//     }
+// };
